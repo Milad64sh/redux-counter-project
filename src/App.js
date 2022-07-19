6e8d57e49f22d88f64f4572173d17a8b969a7462
Milad64sh <m.shalikarian@gmail.com>
2022-07-19 16:34:55 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import Counter from './Counter';
+import { createStore } from 'redux';
+import { DECREASE, RESET, INCREASE } from './actions';
+import reducer from './reducers';
+
+// setup initial state
+const defaultState = {
+  count: 76,
+  name: 'bob',
+};
+// setup reducer
+
+// setup store
+const store = createStore(reducer, defaultState);
+store.dispatch({ type: RESET });
+store.dispatch({ type: DECREASE });
+store.dispatch({ type: INCREASE });
 const App = () => {
-  return <Counter />;
+  return <Counter state={store.getState()} />;
 };
 
 export default App;
---
+++ src/reducers.js
@@ -0,0 +1,16 @@
+import { DECREASE, RESET, INCREASE } from './actions';
+export default function reducer(state, action) {
+  // if (action.type === 'DECREASE') {
+  //   return { ...state, count: state.count - 1 };
+  // }
+  switch (action.type) {
+    case DECREASE:
+      return { ...state, count: state.count - 1 };
+    case INCREASE:
+      return { ...state, count: state.count + 1 };
+    case RESET:
+      return { ...state, count: 0 };
+    default:
+      return state;
+  }
+}
