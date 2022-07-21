79424ed1b5eed1e6b53294e11ebc67397f20b17e
Milad64sh <m.shalikarian@gmail.com>
2022-07-21 11:06:34 -0700

    setting modal

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { DECREASE, RESET, INCREASE } from './actions';
+import { DECREASE, RESET, INCREASE, MODAL_OPEN, MODAL_CLOSE } from './actions';
 
 function Counter({ name, count, increase, decrease, reset }) {
   return (
@@ -29,7 +29,16 @@ function mapDispatchToProps(dispatch, ownProps) {
   return {
     increase: () => dispatch({ type: INCREASE }),
     decrease: () => dispatch({ type: DECREASE }),
-    reset: () => dispatch({ type: RESET }),
+    reset: () => {
+      dispatch({ type: RESET });
+      dispatch({
+        type: MODAL_OPEN,
+        payload: {
+          name: 'susan',
+          text: '      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus, libero reiciendis. Veritatis iure accusantium obcaecati nostrum voluptas, rem minus nisi.',
+        },
+      });
+    },
   };
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/Modal.js
+++ src/Modal.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-const Modal = (props) => {
-  const isOpen = true;
+import { connect } from 'react-redux';
+import { MODAL_CLOSE } from './actions';
+const Modal = ({ isOpen, name, text, close }) => {
+  // const isOpen = true;
   return (
     <div className={`modal-overlay ${isOpen ? `isModalOpen` : null}`}>
-      <div className='modal-container'></div>
-      <h4>some random name</h4>
-      <p>some random text</p>
-      <button className='btn btn-primary'>close</button>
+      <div className='modal-container'>
+        <h4>{name}</h4>
+        <p>{text}</p>
+        <button className='btn btn-primary' onClick={close}>
+          close
+        </button>
+      </div>
     </div>
   );
 };
 
-Modal.propTypes = {};
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  name: PropTypes.string,
+  text: PropTypes.string.isRequired,
+  close: PropTypes.func.isRequired,
+};
+const mapStateToProps = ({ modalState: { isOpen, name, text } }) => {
+  return { isOpen, name, text };
+};
+const mapDispatchToProps = (dispatch) => {
+  return { close: () => dispatch({ type: MODAL_CLOSE }) };
+};
 
-export default Modal;
+export default connect(mapStateToProps, mapDispatchToProps)(Modal);
